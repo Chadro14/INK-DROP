e808064e47696eb04adc3b8321c2d04e6f671f3e
Altesse Royal <altessemwamboko@gmail.com>
2026-07-19 22:56:32 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
+  images: {
+    domains: [
+      'inkdrop-backend.vercel.app',
+      'i.pinimg.com',
+      'slbosebjvnotrifwhbrl.supabase.co',
+    ],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = withPWA(nextConfig);
