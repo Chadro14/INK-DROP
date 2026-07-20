d0ad78970061d68d415c8c877440120af48abac6
Altesse Royal <altessemwamboko@gmail.com>
2026-07-20 04:52:01 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
   swcMinify: true,
   output: 'standalone',
   experimental: {
-    optimizeCss: true,
+    optimizeCss: false, // ← DÉSACTIVÉ
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production' ? {
